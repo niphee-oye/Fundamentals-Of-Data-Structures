a791c5a4ba4d88dfee6dcc78cd05f05bb08a3f28
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-05-25 10:52:10 +0100

    Traversing arrays

---
+++ Traversing Arrays/traversing_arrays.java
@@ -0,0 +1,18 @@
+public class traversing_arrays {
+
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        System.out.println("");
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Element at index " + i + " is: " + array[i]);
+        }
+        System.out.println("");
+    }
+}
